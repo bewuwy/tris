9dcebdd09c228be53f9f781dad82c42a3bb23052
Bartek <61749397+bewuwy@users.noreply.github.com>
2024-10-07 15:56:08 +0200

    printing board

--- src/game/game.s
+++ src/game/game.s
@@ -19,6 +19,16 @@ along with gamelib-x64. If not, see <http://www.gnu.org/licenses/>.
 
 .file "src/game/game.s"
 
+.data
+
+score: .int 0
+initBoard: .quad 0x8181818181818181
+currentBoard: .quad 0x8181818181818181
+
+cornerPieces: .quad 0x0620064002600460
+
+.text
+
 .global gameInit
 .global gameLoop
 
@@ -30,22 +40,53 @@ gameInit:
 	ret
 
 gameLoop:
-	# Check if a key has been pressed
-	call	readKeyCode
-	cmpq	$0, %rax
-	je		1f
-	# If so, print a 'Y'
-	movb	$'Y', %dl
-	jmp		2f
 
-1:
-	# Otherwise, print a 'N'
-	movb	$'N', %dl
+	# get current board to r15
+	movq $currentBoard, %r8
+	movq (%r8), %r15
 
-2:
-	movq	$0, %rdi
-	movq	$0, %rsi
-	movb	$0x0f, %cl
-	call	putChar
+	movq $7, %r8  # i = 7
+
+	print_loop:
+		movq $7, %r9  # j = 7
+
+
+		print_row_loop:
+
+			# print num at (i, j)
+
+			movq $0, %rdx  # clear rdx
+			movq %r15, %rax  # move board to rax for division
+			movq $2, %rcx
+			divq %rcx  # divide board by 2
+
+			cmpq $0, %rdx  # rdx = modulo of 2
+			jne print1  # if not divisible by 2, print 1
+
+			print0:
+				movb $'0', %dl  # 0 char
+				movb $0x00, %cl  # colour
+
+				jmp end_print_iter
+
+			print1:
+				movb $'B', %dl  # B char
+				movb $0x0f, %cl  # white colour
+
+			end_print_iter:
+
+			movq %r9, %rdi  # x = j
+			movq %r8, %rsi  # y = i
+			call putChar # print char
+
+			shr %r15  # shift board to get next bit
+
+			dec %r9
+			jge print_row_loop
+		end_print_row_loop:
+
+		dec %r8
+		jge print_loop
+	end_print_loop:
 
 	ret
